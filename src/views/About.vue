61f26c01b7e2b882d612933f3a7a2f54445d0c9f
Matheus F. Dias <57161520+Foscariny@users.noreply.github.com>
2022-06-20 22:50:57 -0300

    feat(About): melhorando e adicionando mais informações no sobre do projeto

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
     <div class="jumbotron m-3">
-      <h1 class="display-4">Sobre</h1>
+      <h1 class="display-4 mb-4">Sobre</h1>
 
       <p class="lead">
         Este projeto foi criado com o fim de aprendizado do poderosíssimo 
@@ -14,15 +14,61 @@
         backend deste CRUD em <b>Ruby on Rails</b>, outra poderosa tecnologia.
       </p>
 
+      <p class="lead">
+        Para acessar o repositório da API usado para este projeto basta acessar este link: 
+        <a href="https://github.com/matheus-foscarinid/rails_orm">API</a>
+      </p>
+
       <hr class="my-4">
 
-      <p class="lead">
+      <p class="lead pt-1 extra">
         P.S: Para mais informações sobre o projeto, acesse meus <a href="https://github.com/matheus-foscarinid">repositórios</a> :)
       </p>
-
-      <p>
-        Att. Matheus Dias
-      </p>
+      
+      <div class="icon-container">
+        <!-- O link é direto do Github mesmo, como é apenas para um projeto de brincadeira -->
+        <img class="icon" src="https://avatars.githubusercontent.com/u/57161520?v=4" />
+        <div class="text-container">
+          <span class="name ml-3">Matheus Dias</span>
+          <span class="title ml-3">Fullstack developer</span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+  .icon-container {
+    background-color: #fff;
+    padding: 10px;
+    border-radius: 10px;
+    width: 20%;
+    display: flex;
+  }
+
+  .text-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+  .icon {
+    border-radius: 50%;
+    width: 70px;
+  }
+
+  .name {
+    font-size: 15px;
+    font-weight: bold;
+    color: #333;
+  }
+
+  .title {
+    font-size: 12px;
+    color: #666;
+  }
+
+  .extra {
+    font-size: 12px;
+    opacity: 0.7;
+  }
+</style>
